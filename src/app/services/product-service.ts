017984283276ce4c8860db3db8bec3b5d7c87cad
Stepan <studiorain27@gmail.com>
2022-06-17 22:10:21 +0300

    Провайдер и модель продукта

--- src/app/services/product-service.ts
+++ src/app/services/product-service.ts
@@ -11,4 +11,21 @@ export class ProductService {
   getProduct(): Product {
     return new Product(0, "Карифская пыльца", 29);
   }
+
+  getProducts(): Array<Product> {
+    return this.products.map(p => new Product(p.id, p.title, p.price));
+  }
+
+  products = [
+    {
+      "id": 0,
+      "title": "First Product",
+      "price": 24.99
+    },
+    {
+      "id": 1,
+      "title": "Second Product",
+      "price": 64.99
+    }
+  ];
 }
